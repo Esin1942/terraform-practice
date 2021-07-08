ad624a6c28d3777dd3ee08d74463302eaba92b2c
Esin1942 <esinyurtdakal@gmail.com>
2021-07-08 17:22:50 +0000

    added more information

--- Class4/variable.tf
+++ Class4/variable.tf
@@ -1,3 +1,12 @@
-variable "region" {}
-variable "key_name" {}
-variable "public_key" {}
+variable "region" {
+  type        = string
+  description = "please provide your region"
+}
+variable "key_name" {
+  type        = string
+  description = "please provide your key_name"
+}
+variable "public_key" {
+  type        = string
+  description = "please provide your public_key"
+}
